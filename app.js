c09724807b66637f596039ec05eec26744e14f63
WesRamox <wesleyramox258@gmail.com>
2024-04-25 19:57:19 -0300

    Melhorias responsividade

---
+++ app.js
@@ -0,0 +1,15 @@
+let hamburgerItem = document.getElementById("hamburgerItem");
+let listItem = document.getElementById("listItems")
+
+let xNav = ""
+
+hamburgerItem.addEventListener('click', () => {
+    if(listItem.classList.contains('actived')) {
+        listItem.classList.remove('actived')
+    } else {
+        listItem.classList.add('actived')
+    }
+    console.log("Oi")
+})
+
+console.log(hamburgerItem)
